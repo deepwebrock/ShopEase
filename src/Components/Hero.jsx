c32dcbb4837e7457b4ec5dd8b2d660f4b9d72c53
deepwebrock <sandeepsingh.1id@gmail.com>
2025-02-23 17:13:28 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
     <section 
       id="hero" 
       className="relative h-screen flex items-center justify-center text-center bg-cover bg-center px-4"
-      style={{ backgroundImage: "url('background.jpg')" }}
+      style={{ backgroundImage: "url('background3.jpg')" }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
